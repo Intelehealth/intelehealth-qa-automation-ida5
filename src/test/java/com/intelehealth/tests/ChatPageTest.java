962fd523ef8b481b2b575b54c2e8c40c722d7d86
srinivas-ih <srinivasbandi@intelehealth.org>
2026-06-29 11:18:56 +0530

    Execute in pathqa server

--- src/test/java/com/intelehealth/tests/ChatPageTest.java
+++ src/test/java/com/intelehealth/tests/ChatPageTest.java
@@ -33,11 +33,13 @@ public class ChatPageTest {
 		basePage = new BasePage();
 		prop = basePage.init_prop();
 		driver = basePage.init_driver(prop);
-		// Initialize  here
+		// Initialize here
 		loginPage = new LoginPage(driver);
-	//	credentials = new Credentials(prop.getProperty("username"), prop.getProperty("password"));
-		credentials = new Credentials(ConfigManager.getInstance().getUsername(), ConfigManager.getInstance().getPassword());
-				dashboardPage = loginPage.doLogin(credentials);
+		// credentials = new Credentials(prop.getProperty("username"),
+		// prop.getProperty("password"));
+		credentials = new Credentials(ConfigManager.getInstance().getUsername(),
+				ConfigManager.getInstance().getPassword());
+		dashboardPage = loginPage.doLogin(credentials);
 		chatPage = new ChatPage(driver);
 		ScreenshotListener.setDriver(driver);
 	}
@@ -46,47 +48,47 @@ public class ChatPageTest {
 	@Description("Verify chat option visibility")
 	@Severity(SeverityLevel.BLOCKER)
 	public void IDA4_1826_Chat() throws InterruptedException {
-		 
-		//System.out.println("Started execution of IDA4_1826");
+
+		// System.out.println("Started execution of IDA4_1826");
 		chatPage.chatOptionvisibility();
 	}
-	
+
 	@Test(priority = 2, description = "IDA4_1828_Verify chat option functionality in visit summary page", enabled = true)
 	@Description("Verify chat option visibility")
 	@Severity(SeverityLevel.BLOCKER)
 	public void IDA4_1828_Chat() throws InterruptedException {
-		 
-		//System.out.println("Started execution of IDA4_1828");
+
+		// System.out.println("Started execution of IDA4_1828");
 		chatPage.chatOptionfunctionality();
 	}
-	
+
 	@Test(priority = 3, description = "IDA4_1830_Verify sending message in chat", enabled = true)
 	@Description("Verify sending message in chat")
 	@Severity(SeverityLevel.BLOCKER)
 	public void IDA4_1830_Chat() throws InterruptedException {
-		 
-		//System.out.println("Started execution of IDA4_1830");
+
+		// System.out.println("Started execution of IDA4_1830");
 		chatPage.chatSendMessage();
 	}
-	
+
 	@Test(priority = 4, description = "IDA4_1833_Verify whether HW able to click on the PN when doctor send a chat msg", enabled = true)
 	@Description("Verify whether HW able to click on the PN when doctor send a chat msg")
 	@Severity(SeverityLevel.BLOCKER)
 	public void IDA4_1833_Chat() throws InterruptedException {
-		 
-		//System.out.println("Started execution of IDA4_1833");
+
+		// System.out.println("Started execution of IDA4_1833");
 		chatPage.chatSendMessagetoHW();
 	}
-	
+
 	@Test(priority = 5, description = "IDA4_1834_Verify whether healthworker able to reply to doctor message/Check the sent message from doctor by logging into mobile", enabled = true)
 	@Description("Verify whether healthworker able to reply to doctor message/Check the sent message from doctor by logging into mobile")
 	@Severity(SeverityLevel.BLOCKER)
 	public void IDA4_1834_Chat() throws InterruptedException {
-		 
-		//System.out.println("Started execution of IDA4_1834");
+
+		// System.out.println("Started execution of IDA4_1834");
 		chatPage.chatSendMessagetoHW();
 	}
-	
+
 	@AfterMethod
 	public void tearDown() throws Throwable {
 		driver.quit();
